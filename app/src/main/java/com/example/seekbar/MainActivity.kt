5aea2945e70a6ab798fe2923d2e8fbf8a92e7cd5
mubarakansari715 <mubarakansari715@gmail.com>
2021-07-20 17:12:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/seekbar/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.seekbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val seekbar = findViewById<SeekBar>(R.id.seekBar)
+        val textView = findViewById<TextView>(R.id.tv_view)
+
+        seekbar.max =100
+        seekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView.text=progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+    }
+}
